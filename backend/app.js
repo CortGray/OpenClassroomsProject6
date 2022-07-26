56b2b237070bde37d1e1f78680b4257f152ec667
Cort Gray <cortkatgray@gmail.com>
2022-07-25 20:32:54 -0700

    started backend server

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://CortGray:2678Gray@cluster0.9sekn.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => {
+        console.log('Successfully connected to MongoDB Atlas.');
+    })
+    .catch((error) => {
+        console.log('Unable to connect.');
+        console.error(error);
+    });
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+module.exports = app; 
