e3a466c540643b10323c6ea41d408581643265b4
Cort Gray <cortkatgray@gmail.com>
2022-07-30 20:18:14 -0700

    second commit attempt, same as previous

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
+
+const userRoutes = require('./routes/user');
 
 const app = express();
 
@@ -20,5 +23,9 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/auth', userRoutes);
 
 module.exports = app; 
